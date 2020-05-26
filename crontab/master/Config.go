f0cbf4518e950b642830605cc3da9d6400b05119
anmutu <anmutu@hotmail.com>
2020-05-26 08:37:01 +0800

    初始化命令行参数及配置等。

---
+++ crontab/master/Config.go
@@ -0,0 +1,40 @@
+/*
+  author='du'
+  date='2020/5/26 7:55'
+*/
+package master
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Config struct {
+	ApiPort         int `json:"apiPort"`
+	ApiReadTimeOut  int `json:apiReadTimeOut`
+	ApiWriteTimeOut int `json:apiWriteTimeOut`
+}
+
+var (
+	G_config *Config //单例对象，指针。
+)
+
+//初始化配置文件
+func InitConfig(filename string) (err error) {
+	var (
+		content []byte
+		conf    Config
+	)
+
+	//把配置文件读进来
+	if content, err = ioutil.ReadFile(filename); err != nil {
+		return
+	}
+	//json反序列化
+	if err = json.Unmarshal(content, &conf); err != nil {
+		return
+	}
+	//赋值给单例config
+	G_config = &conf
+	return
+}
--- crontab/master/main/master.go
+++ crontab/master/main/master.go
@@ -6,14 +6,27 @@ package main
 
 import (
 	"du_corntab/crontab/master"
+	"flag"
 	"fmt"
 	"runtime"
+)
+
+var (
+	confFile string
 )
 
 func main() {
 	var (
 		err error
 	)
+
+	//初始化命令行参数
+	initArgs()
+
+	//初始化配置,这里的confFile就是通过initArgs得到的。
+	if err = master.InitConfig(confFile); err != nil {
+		goto ERR
+	}
 
 	//初始化线程
 	initEnv()
@@ -31,4 +44,10 @@ ERR:
 
 func initEnv() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+//解析命令行参数，最后将之放到配置文件路径中
+func initArgs() {
+	flag.StringVar(&confFile, "conf", "master.json", "指定master.json")
+	flag.Parse()
 }
