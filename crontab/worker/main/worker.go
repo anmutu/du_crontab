2dd2d37a0e5afaff3c6e916dfb50eef95aff4b8c
anmutu <anmutu@hotmail.com>
2020-05-29 23:44:54 +0800

    fix,调通worker的监视功能。

--- crontab/worker/main/worker.go
+++ crontab/worker/main/worker.go
@@ -55,6 +55,6 @@ func initEnv() {
 
 //解析命令行参数，最后将之放到配置文件路径中
 func initArgs() {
-	flag.StringVar(&confFile, "conf", "master.json", "指定master.json")
+	flag.StringVar(&confFile, "conf", "worker.json", "worker.json")
 	flag.Parse()
 }
