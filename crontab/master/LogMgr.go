dee4b3cb7dfb6e2deaded08ee1ddaf2b50c0f109
anmutu <anmutu@hotmail.com>
2020-06-07 23:44:14 +0800

    日志列表根据执行时间倒序等优化。

--- crontab/master/LogMgr.go
+++ crontab/master/LogMgr.go
@@ -43,10 +43,10 @@ func InitLogMgr() (err error) {
 // 查看任务日志
 func (logMgr *LogMgr) GetLoglist(name string, skip int, limit int) (logArr []*common.JobLog, err error) {
 	var (
-		//filter *common.JobLogFilter
-		logSort *common.SortLogByStartTime
-		//cursor *mongo.Cursor
-		jobLog *common.JobLog
+	//filter *common.JobLogFilter
+	//logSort *common.SortLogByStartTime
+	//cursor *mongo.Cursor
+	//jobLog *common.JobLog
 	)
 
 	// len(logArr)
@@ -57,60 +57,32 @@ func (logMgr *LogMgr) GetLoglist(name string, skip int, limit int) (logArr []*co
 	//filter = &common.JobLogFilter{}
 
 	// 按照任务开始时间倒排
-	logSort = &common.SortLogByStartTime{SortOrder: -1}
+	//logSort = &common.SortLogByStartTime{SortOrder: -1}
 
 	// 查询
-	ops := options.Find().SetSort(logSort).SetLimit(int64(limit)).SetSkip(int64(skip))
-	ops = ops
-	//ops1:=options.Find()
+	//ops := options.Find().SetSort(logSort).SetLimit(int64(limit)).SetSkip(int64(skip))
+	//ops = ops
 
-	//if cursor,err=logMgr.logCollection.Find(context.TODO(),filter);err!=nil{
-	//	return nil, err
-	//}
-
-	//filter = bson.{"jobName": name}
 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	//cur, err := logMgr.logCollection.Find(ctx,base.filter,ops)
-	//cur, err := logMgr.logCollection.Find(ctx,base)
-	cur, err := logMgr.logCollection.Find(ctx, bson.D{})
+
+	opts := options.Find().SetSort(bson.D{{"scheduleTime", -1}}).SetLimit(100)
+	cur, err := logMgr.logCollection.Find(context.TODO(), bson.D{{"jobName", name}}, opts)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer cur.Close(ctx)
 	for cur.Next(ctx) {
 		//var result bson.M
-		jobLog = &common.JobLog{}
+		jobLog := &common.JobLog{}
 		err := cur.Decode(jobLog)
 		if err != nil {
 			log.Fatal(err)
 		}
-		// do something with result....
-		//if(jobLog.JobName==name){
 		logArr = append(logArr, jobLog)
-		//}
 	}
 	if err := cur.Err(); err != nil {
 		log.Fatal(err)
 	}
-
-	//opts := options.Find().SetSort(bson.D{{"startTime", -1}})
-	//cursor, err = logMgr.logCollection.Find(context.TODO(), bson.D{{"jobName", name}}, opts)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//var results []bson.M if err = cursor.All(context.TODO(), &results); err != nil {     log.Fatal(err) } for _, result := range results {     fmt.Println(result) }
-
-	//defer cursor.Close(context.TODO())
-	//
-	//for cursor.Next(context.TODO()) {
-	//	jobLog = &common.JobLog{}
-	//	if err = cursor.Decode(jobLog); err != nil {
-	//		continue // 日志不合法也继续
-	//	}
-	//	logArr = append(logArr, jobLog)
-	//	fmt.Println("len(logArr):",len(logArr))
-	//}
-	//fmt.Println("len(logArr):",len(logArr))
 	return
 }
